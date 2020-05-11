3f3a7d98496c90a134d0d443e53e489e509670a6
Beedeeboom <stephanie.duffieux@gmail.com>
2020-05-11 17:59:57 +1000

    added Artwork scaffold, Pages controller, routes and  styling with Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+  root to: 'pages#index'
+  get 'pages/about', to: 'pages#about', as: "about"
+  get 'pages/contact', to: "pages#contact", as: "contact"
+  resources :artworks
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
